60a10f226085d9f282ffaa96ae9ba6a207434bef
Christian Gajardo <christian.gajardo@sellside.cl>
2026-02-01 23:16:02 +0000

    Se creo el modulo ai_cart_integration

---
+++ ai_cart_integration/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'AI Chat to Cart Integration',
+    'version': '0-1',
+    'category': 'Website',
+    'depends': [
+        'website_sale',
+        'im_livechat'],
+    'data': [
+    ],
+    'installable': True,
+    'license': 'OEEL-1',
+}
