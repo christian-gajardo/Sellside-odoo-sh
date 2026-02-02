8d818c02779b5126aa9175853682d4c130e9331e
Christian <christian.gajardo@sellside.cl>
2026-02-02 22:25:52 +0000

    Actualizacion de manifest

--- ai_cart_integration/__manifest__.py
+++ ai_cart_integration/__manifest__.py
@@ -1,12 +1,18 @@
 {
     'name': 'AI Chat to Cart Integration',
     'version': '0.1',
-    'category': 'Website',
+    'summary': 'Permite al agente de IA añadir productos al carrito directamente desde el chat.',
+    'author': 'IA-Sellside-ChG',
     'depends': [
         'website_sale',
-        'im_livechat'],
+        'im_livechat',
+        'sale',
+        'ai',
+        'ai_app',
+    ],
     'data': [
     ],
     'installable': True,
+    'application': True,
     'license': 'OEEL-1',
 }
