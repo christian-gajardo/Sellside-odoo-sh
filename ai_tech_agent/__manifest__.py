411e3c42716f4fb0f895f830e75279eeff8f490a
Christian Gajardo <c.gajardo08@ufromail.cl>
2026-02-03 21:05:22 -0300

    chore(manifest): añadir crm a las dependencias.

--- ai_tech_agent/__manifest__.py
+++ ai_tech_agent/__manifest__.py
@@ -4,7 +4,8 @@
     'summary': 'Permite al agente de IA añadir productos al carrito directamente desde el chat.',
     'author': 'IA-Sellside-ChG',
     'depends': [
-        'website',
+        'website_sale',
+        'crm',
         'im_livechat',
         'stock',
         'ai',
